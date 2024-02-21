cce3c01d7886d75b3b4ec8e8b184378507b61b68
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-02-20 23:08:46 -0300

    Add project files. Add Category and Transaction entities

---
+++ ExpenseTrackerMvc/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTrackerMvc.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public Category Category { get; set; }
+        public int Amount { get; set; }
+        public string Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
