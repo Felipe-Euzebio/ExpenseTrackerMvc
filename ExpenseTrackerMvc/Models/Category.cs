e0dae86dcfef282bfbd4738fc851a9d10ff09f2f
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-02-20 23:34:53 -0300

    Add custom props to some columns of Category and Transaction entities. Add InitialCreate migration

--- ExpenseTrackerMvc/Models/Category.cs
+++ ExpenseTrackerMvc/Models/Category.cs
@@ -1,10 +1,18 @@
-﻿namespace ExpenseTrackerMvc.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerMvc.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
         public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
         public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
     }
 }
--- ExpenseTrackerMvc/Models/Transaction.cs
+++ ExpenseTrackerMvc/Models/Transaction.cs
@@ -1,11 +1,16 @@
-﻿namespace ExpenseTrackerMvc.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerMvc.Models
 {
     public class Transaction
     {
         public int Id { get; set; }
         public Category Category { get; set; }
         public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
+
         public DateTime Date { get; set; } = DateTime.Now;
     }
 }
